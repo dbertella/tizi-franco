4808f345819cd7bb36a25792ddc63903de4a6701
Daniele Bertella <daniele.bertella@gmail.com>
2020-07-24 22:22:52 +0100

    Gatsby to ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Card, Heading } from "theme-ui"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Tiziana E Franco Si Sposano" />
+    <Card>
+      <Heading as="h3">È Ufficiale</Heading>
+      <Heading sx={{ variant: "caps" }}>Tiziana E Franco Si Sposano</Heading>
+    </Card>
+  </>
+)
+
+export default IndexPage
